cae10e993ce5bc30f41d5f666f5653635d8ad9a7
Alok Singh <thealoksinghh@gmail.com>
2026-01-22 16:39:48 +0530

    url changed

--- backend/database/ConnectDB.js
+++ backend/database/ConnectDB.js
@@ -5,9 +5,8 @@ dotenv.config();
 
 const ConnectDB = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO_URI);
-     await mongoose.connect("mongodb+srv://vaishalitechinfo_db_user:ATGoOusKCwAxYUgH@cluster0.omusakb.mongodb.net/");
-    console.log("MongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+     console.log("MongoDB connected successfully");
     } catch (error) {
     console.error("MongoDB connection failed:", error);
     process.exit(1);
