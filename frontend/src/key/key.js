182052f7bfb6ccb27126157f4b3969f957ad1497
Alok Singh <thealoksinghh@gmail.com>
2026-01-22 16:34:14 +0530

    key changed

--- frontend/src/key/key.js
+++ frontend/src/key/key.js
@@ -1,15 +1,15 @@
-// export const emailKeys = {
-//   serviceId: "service_n2966z8",
-//   templateId: "template_1rzu4vd",
-//   publicKey: "emp2B6knUFfGwfYzl",
-  
-// };
 export const emailKeys = {
-  serviceId: "testing_service",
-  templateId: "testing_template",
-  publicKey: "testing_key",
+  serviceId: "service_n2966z8",
+  templateId: "template_1rzu4vd",
+  publicKey: "emp2B6knUFfGwfYzl",
   
 };
+// export const emailKeys = {
+//   serviceId: "testing_service",
+//   templateId: "testing_template",
+//   publicKey: "testing_key",
+  
+// };
 export const credentials={
   web_url: "satyammetroshowstoppers.in",
   web_name: "Satyam Metro Showstoppers",
